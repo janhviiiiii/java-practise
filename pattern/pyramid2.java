aac936fff0f7d8970afd0ac75182f1af14714352
janhviiiiii <janhvibaraskar03@gmail.com>
2024-11-14 00:11:44 +0530

    added new file

--- pattern/pyramid2.java
+++ pattern/pyramid2.java
@@ -1,5 +1,24 @@
 package pattern;
 
+import java.util.Scanner;
+
 public class pyramid2 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of row/columns: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i <= j) {
+                    System.out.print("*" + " ");
+                } else {
+                    System.out.print(" " + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
 }
